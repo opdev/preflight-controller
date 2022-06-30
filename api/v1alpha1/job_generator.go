8c29f886471746016b78462e7c64f00656f4b315
Jose R. Gonzalez <jose@flutes.dev>
2022-06-30 13:47:52 -0500

    wire up credentials to ensure data on disk is capture as env vars
    Signed-off-by: Jose R. Gonzalez <jose@flutes.dev>

--- api/v1alpha1/job_generator.go
+++ api/v1alpha1/job_generator.go
@@ -1,16 +1,23 @@
 package v1alpha1
 
 import (
+	"path"
+
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
-	kubeconfigSecretKey            = "kubeconfig"
-	dockerConfigJSONSecretKey      = "config.json"
-	defaultDockerConfigSecretName  = "dockerconfig"
-	defaultPyxisAPITokenSecretName = "pyxisapi"
+	kubeconfigSecretKey  = "kubeconfig"
+	kubeconfigVolumeName = "kubeconfig"
+
+	dockerConfigJSONSecretKey  = "config.json"
+	dockerConfigJSONVolumeName = "dockerconfigjson"
+
+	defaultVolumeMountPath = "/preflight"
+
+	pyxisAPITokenSecretKey = "pyxisapitoken"
 )
 
 // PreflightCheckJobGenerator generates a batchv1.Job for a
@@ -19,238 +26,35 @@ type PreflightCheckJobGenerator struct {
 	pc PreflightCheck
 }
 
-// preflightImage returns the value for Spec.preflightImage in pc,
-// or the default value of quay.io/opdev/preflight:stable.
-func (g *PreflightCheckJobGenerator) preflightImage() string {
-	img := "quay.io/opdev/preflight:stable"
-	if g.pc.Spec.PreflightImage != nil {
-		img = *g.pc.Spec.PreflightImage
-	}
-
-	return img
-}
-
-// envLogLevel returns the value for Spec.LogLevel in pc, or
-// a default value of Info.
-func (g *PreflightCheckJobGenerator) envLogLevel() corev1.EnvVar {
-	if g.pc.Spec.LogLevel != nil {
-		return corev1.EnvVar{
-			Name:  "PFLT_LOGLEVEL",
-			Value: *g.pc.Spec.LogLevel,
-		}
-	}
-
-	return corev1.EnvVar{
-		Name:  "PFLT_LOGLEVEL",
-		Value: "Info", // defaulting to Info
-	}
-}
-
-// envCertificationProjectID returns the value for
-// Spec.CheckContainerOtions.CertificationProjectID in g.pc. Because this
-// is optional, this returns a pointer.
-//
-// a nil value indicates that this is not set in g.pc.
-func (g *PreflightCheckJobGenerator) envCertificationProjectID() *corev1.EnvVar {
-	if g.pc.Spec.CheckOptions.ContainerOptions != nil {
-		if g.pc.Spec.CheckOptions.ContainerOptions.CertificationProjectID != nil {
-			return &corev1.EnvVar{
-				Name:  "PFLT_CERTIFICATION_PROJECT_ID",
-				Value: *g.pc.Spec.CheckOptions.ContainerOptions.CertificationProjectID,
-			}
-		}
-	}
-
-	return nil
-}
-
-func (g *PreflightCheckJobGenerator) envIndexImage() *corev1.EnvVar {
-	if g.pc.Spec.CheckOptions.OperatorOptions != nil {
-		return &corev1.EnvVar{
-			Name:  "PFLT_INDEXIMAGE",
-			Value: g.pc.Spec.CheckOptions.OperatorOptions.IndexImage,
-		}
-	}
-
-	return nil
-}
-
-// envKubeconfig returns a kubeconfig environment variable that references
-// a volume-mounted secret. If nil, it's assumed that we don't need this
-// environment variable.
-func (g *PreflightCheckJobGenerator) envKubeconfig() *corev1.EnvVar {
-	isOptional := false
-
-	if g.pc.Spec.CheckOptions.OperatorOptions != nil {
-		return &corev1.EnvVar{
-			Name: "KUBECONFIG",
-			ValueFrom: &corev1.EnvVarSource{
-				SecretKeyRef: &corev1.SecretKeySelector{
-					LocalObjectReference: corev1.LocalObjectReference{
-						Name: "kubeconfig",
-					},
-					Key:      kubeconfigSecretKey,
-					Optional: &isOptional,
-				},
-			},
-		}
-	}
-
-	return nil
-}
-
-// volumeKubeconfig returns the volume configuration for the preflight kubecofig
-// based on the secret reference provided by the user.
-func (g *PreflightCheckJobGenerator) volumeKubeconfig() *corev1.Volume {
-	isOptional := false
-
-	if g.pc.Spec.CheckOptions.OperatorOptions != nil {
-		return &corev1.Volume{
-			Name: "kubeconfig",
-			VolumeSource: corev1.VolumeSource{
-				Secret: &corev1.SecretVolumeSource{
-					SecretName: g.pc.Spec.CheckOptions.OperatorOptions.KubeconfigSecretRef,
-					Optional:   &isOptional,
-				},
-			},
-		}
-	}
-
-	return nil
-}
-
-// volumeDockerConfigJSON returns the volume configuration for the DockerConfigJSON
-// based on the secret reference provided by the user, or a default value.
-func (g *PreflightCheckJobGenerator) volumeDockerConfigJSON() corev1.Volume {
-	isOptional := true
-
-	secretName := defaultDockerConfigSecretName
-	if g.pc.Spec.DockerConfigSecretRef != nil {
-		secretName = *g.pc.Spec.DockerConfigSecretRef
-	}
-
-	return corev1.Volume{
-		Name: "dockerconfigjson",
-		VolumeSource: corev1.VolumeSource{
-			Secret: &corev1.SecretVolumeSource{
-				SecretName: secretName,
-				Optional:   &isOptional,
-			},
-		},
-	}
-}
-
-// volumePyxisAPIToken returns the volume configuration for the PyxisAPIToken
-// based on the secret reference provided by the user, or a default value.
-func (g *PreflightCheckJobGenerator) volumePyxisAPIToken() corev1.Volume {
-	isOptional := true
-
-	secretName := defaultPyxisAPITokenSecretName
-	if g.pc.Spec.CheckOptions.ContainerOptions != nil {
-		if g.pc.Spec.CheckOptions.ContainerOptions.PyxisAPITokenSecretRef != nil {
-			secretName = *g.pc.Spec.CheckOptions.ContainerOptions.PyxisAPITokenSecretRef
-		}
-	}
-
-	return corev1.Volume{
-		Name: "pyxisapitoken",
-		VolumeSource: corev1.VolumeSource{
-			Secret: &corev1.SecretVolumeSource{
-				SecretName: secretName,
-				Optional:   &isOptional,
-			},
-		},
-	}
-}
-
-// Volumes returns all volumes that would be configured for the job
-// based on the PreflightCheck.Spec.
-func (g *PreflightCheckJobGenerator) Volumes() []corev1.Volume {
-	vols := []corev1.Volume{
-		g.volumeDockerConfigJSON(),
-		g.volumePyxisAPIToken(),
-	}
-
-	// Kubeconfig only applies to the operator check, so
-	// only add it if that's the requested command.
-	if v := g.volumeKubeconfig(); v != nil {
-		vols = append(vols, *v)
-	}
-
-	return vols
-}
-
-// Env returns the environment variables that would be configured
-// for the job based on the PreflightCheck.Spec.
-func (g *PreflightCheckJobGenerator) Env() []corev1.EnvVar {
-	envs := []corev1.EnvVar{
-		g.envLogLevel(),
-		g.envDockerConfigValue(),
-	}
-
-	// Add optional environments.
-	if e := g.envCertificationProjectID(); e != nil {
-		envs = append(envs, *e)
-	}
-
-	if e := g.envKubeconfig(); e != nil {
-		envs = append(envs, *e)
-	}
-
-	if e := g.envIndexImage(); e != nil {
-		envs = append(envs, *e)
-	}
-
-	return envs
-}
-
-// envDockerConfigValue will return the corev1.EnvVar for PFLT_DOCKERCONFIG.
-func (g *PreflightCheckJobGenerator) envDockerConfigValue() corev1.EnvVar {
-	isOptional := true
-	return corev1.EnvVar{
-		Name: "PFLT_DOCKERCONFIG",
-		ValueFrom: &corev1.EnvVarSource{
-			SecretKeyRef: &corev1.SecretKeySelector{
-				LocalObjectReference: corev1.LocalObjectReference{
-					Name: g.dockerConfigSecretRef(),
-				},
-				Key:      dockerConfigJSONSecretKey,
-				Optional: &isOptional, // TODO if it doesn't exist, do we still get the env with an empty string?
-			},
-		},
-	}
-}
-
-// dockerConfigSecretRef returns a dockerConfigSecret name from the
-// PreflightCheck.Spec if set, or a default value if not.
-func (g *PreflightCheckJobGenerator) dockerConfigSecretRef() string {
-	ref := defaultDockerConfigSecretName
-	if g.pc.Spec.DockerConfigSecretRef != nil {
-		ref = *g.pc.Spec.DockerConfigSecretRef
-	}
-
-	return ref
-}
-
-// checkCommand resolves the command to provide to `preflight check` based on
-// the PreflightCheck.Spec.CheckOptions. If the OperatorOptions are set, this will
-// return the operator, and otherwise fall back to container.
-func (g *PreflightCheckJobGenerator) checkCommand() string {
-	cmd := "container"
-
-	if g.pc.Spec.CheckOptions.OperatorOptions != nil {
-		cmd = "operator"
-	}
-
-	return cmd
-}
-
-// TODO(komish): Wire up credentials so that the environment variables refer to
-// the path on the filesystem where the mounted secret volumes exist.
-
 // Generate returns a batchv1.Job based on the input g.PreflightCheck.
 func (g *PreflightCheckJobGenerator) Generate() batchv1.Job {
 	var backoffLimit int32 = 0
+
+	// the baseContainer we're working with. Needs modifications to args, volumeMounts, etc.
+	container := &corev1.Container{
+		Name:         "preflight",
+		Image:        g.preflightImage(),
+		Args:         []string{"check", g.checkCommand(), g.pc.Spec.Image},
+		VolumeMounts: []corev1.VolumeMount{},
+	}
+
+	// base set of volumes we'll use in the PodSpecTemplate
+	volumes := []corev1.Volume{}
+
+	// configure the runtime environment based on the spec.
+	container, volumes = g.configureDockerConfigJSON(container, volumes)
+	container = g.configureLogLevel(container)
+
+	// configure check container options if necessary.
+	if g.pc.Spec.CheckOptions.ContainerOptions != nil {
+		container = g.configurePyxisAPIToken(container)
+		container = g.configureCertificationProjectID(container)
+	}
+
+	// configure check operator options if necessary.
+	if g.pc.Spec.CheckOptions.OperatorOptions != nil {
+		container = g.configureIndexImage(container)
+	}
 
 	return batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
@@ -273,16 +77,184 @@ func (g *PreflightCheckJobGenerator) Generate() batchv1.Job {
 				Spec: corev1.PodSpec{
 					RestartPolicy: corev1.RestartPolicyNever,
 					Containers: []corev1.Container{
-						{
-							Name:  "preflight",
-							Image: g.preflightImage(),
-							Args:  []string{"check", g.checkCommand(), g.pc.Spec.Image},
-							Env:   g.Env(),
-						},
+						*container,
 					},
-					Volumes: g.Volumes(),
+					Volumes: volumes,
 				},
 			},
 		},
 	}
+}
+
+// preflightImage returns the value for Spec.preflightImage in pc,
+// or the default value of quay.io/opdev/preflight:stable.
+func (g *PreflightCheckJobGenerator) preflightImage() string {
+	if g.pc.Spec.PreflightImage != nil {
+		return *g.pc.Spec.PreflightImage
+	}
+
+	return "quay.io/opdev/preflight:stable"
+}
+
+// configureLogLevel add the user's log level preference to the pod, or defaults to Info
+// if none is provided.
+func (g *PreflightCheckJobGenerator) configureLogLevel(c *corev1.Container) *corev1.Container {
+	loglevel := "Info"
+
+	if g.pc.Spec.LogLevel != "" {
+		loglevel = g.pc.Spec.LogLevel
+	}
+
+	c.Env = append(c.Env, corev1.EnvVar{
+		Name:  "PFLT_LOGLEVEL",
+		Value: loglevel,
+	})
+
+	return c
+}
+
+func (g *PreflightCheckJobGenerator) configureCertificationProjectID(c *corev1.Container) *corev1.Container {
+	if g.pc.Spec.CheckOptions.ContainerOptions != nil {
+		if g.pc.Spec.CheckOptions.ContainerOptions.CertificationProjectID != nil {
+			c.Env = append(c.Env, corev1.EnvVar{
+				Name:  "PFLT_CERTIFICATION_PROJECT_ID",
+				Value: *g.pc.Spec.CheckOptions.ContainerOptions.CertificationProjectID,
+			})
+		}
+	}
+
+	return c
+}
+
+func (g *PreflightCheckJobGenerator) configureIndexImage(c *corev1.Container) *corev1.Container {
+	if g.pc.Spec.CheckOptions.OperatorOptions != nil {
+		c.Env = append(c.Env, corev1.EnvVar{
+			Name:  "PFLT_INDEXIMAGE",
+			Value: g.pc.Spec.CheckOptions.OperatorOptions.IndexImage,
+		})
+	}
+
+	return c
+}
+
+// checkCommand resolves the command to provide to `preflight check` based on
+// the PreflightCheck.Spec.CheckOptions. If the OperatorOptions are set, this will
+// return the operator, and otherwise fall back to container.
+func (g *PreflightCheckJobGenerator) checkCommand() string {
+	cmd := "container"
+
+	if g.pc.Spec.CheckOptions.OperatorOptions != nil {
+		cmd = "operator"
+	}
+
+	return cmd
+}
+
+// configurePyxisAPIToken will add the pyxisAPIToken value to the environment from the secret ref.
+func (g *PreflightCheckJobGenerator) configurePyxisAPIToken(c *corev1.Container) *corev1.Container {
+	if g.pc.Spec.CheckOptions.ContainerOptions == nil {
+		// The job being executed is not a container check.
+		return c
+	}
+
+	if g.pc.Spec.CheckOptions.ContainerOptions.PyxisAPITokenSecretRef == "" {
+		// The user didn't include a secret to check.
+		return c
+	}
+
+	// Add the secret to the volume list.
+	notOptional := false
+
+	// Project the mountpoint as the environment variable.
+	c.Env = append(c.Env, corev1.EnvVar{
+		Name: "PFLT_PYXIS_API_TOKEN",
+		ValueFrom: &corev1.EnvVarSource{
+			SecretKeyRef: &corev1.SecretKeySelector{
+				LocalObjectReference: corev1.LocalObjectReference{
+					Name: g.pc.Spec.CheckOptions.ContainerOptions.PyxisAPITokenSecretRef,
+				},
+				Key:      pyxisAPITokenSecretKey,
+				Optional: &notOptional,
+			},
+		},
+	})
+
+	return c
+}
+
+// configureKubeconfig will mount the provided kubeconfig secret to the v and configure the KUBECONFIG
+// environment variable to c
+func (g *PreflightCheckJobGenerator) configureKubeconfig(c *corev1.Container, v []corev1.Volume) (*corev1.Container, []corev1.Volume) {
+	if g.pc.Spec.CheckOptions.OperatorOptions == nil {
+		// The spec indicates that we're not running operator checks
+		return c, v
+	}
+
+	if g.pc.Spec.CheckOptions.OperatorOptions.KubeconfigSecretRef == "" {
+		// The user didn't include a secret to check.
+		return c, v
+	}
+
+	// Add the secret to the volume list.
+	notOptional := false
+	v = append(v, corev1.Volume{
+		Name: kubeconfigVolumeName,
+		VolumeSource: corev1.VolumeSource{
+			Secret: &corev1.SecretVolumeSource{
+				SecretName: g.pc.Spec.CheckOptions.OperatorOptions.KubeconfigSecretRef,
+				Optional:   &notOptional,
+			},
+		},
+	})
+
+	// Mount the secret to the container.
+	c.VolumeMounts = append(c.VolumeMounts, corev1.VolumeMount{
+		Name:      kubeconfigVolumeName,
+		ReadOnly:  true,
+		MountPath: defaultVolumeMountPath,
+	})
+
+	// Project the mountpoint as the environment variable.
+	c.Env = append(c.Env, corev1.EnvVar{
+		Name:  "KUBECONFIG",
+		Value: path.Join(defaultVolumeMountPath, kubeconfigSecretKey),
+	})
+
+	return c, v
+}
+
+// configureDockerConfigJSON will add the dockerConfigSecretRef as a volume to v, as
+// a volumeMount to c, and as the PFLT_DOCKERCONFIG environment variable to c.
+func (g *PreflightCheckJobGenerator) configureDockerConfigJSON(c *corev1.Container, v []corev1.Volume) (*corev1.Container, []corev1.Volume) {
+	if g.pc.Spec.DockerConfigSecretRef == "" {
+		// The user didn't include a secret to check.
+		return c, v
+	}
+
+	// Add the secret to the volume list.
+	notOptional := false
+	v = append(v, corev1.Volume{
+		Name: dockerConfigJSONVolumeName,
+		VolumeSource: corev1.VolumeSource{
+			Secret: &corev1.SecretVolumeSource{
+				SecretName: g.pc.Spec.DockerConfigSecretRef,
+				Optional:   &notOptional,
+			},
+		},
+	})
+
+	// Mount the secret to the container.
+	c.VolumeMounts = append(c.VolumeMounts, corev1.VolumeMount{
+		Name:      dockerConfigJSONVolumeName,
+		ReadOnly:  true,
+		MountPath: defaultVolumeMountPath},
+	)
+
+	// Project the mountpoint as the environment variable.
+	c.Env = append(c.Env, corev1.EnvVar{
+		Name:  "PFLT_DOCKERCONFIG",
+		Value: path.Join(defaultVolumeMountPath, dockerConfigJSONSecretKey),
+	})
+
+	return c, v
 }
